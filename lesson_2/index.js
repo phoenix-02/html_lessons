4a057a95e8269b64f684b95043a9f58e515bc120
phoenix-02 <50809139+phoenix-02@users.noreply.github.com>
2021-01-23 00:17:36 +0600

    исправлено отображение даты

--- lesson_2/index.js
+++ lesson_2/index.js
@@ -6,14 +6,16 @@ window.onload = function () {
     const todo_list = document.querySelector('.todo_list');
     const root = document.querySelector('#root');
     let date = new Date
+    todo_list.querySelector("time").innerText = (date+'').slice(0,15)
 
 
     for (let i = 1; i < 7; i++) {
         /*так мы клонируем  элемент todo_list */
-        date.setDate(date.getDate() + 1 )
         let cloneElement = todo_list.cloneNode(true)
-        /*так мы вставляем клоны в root */
         root.appendChild(cloneElement)
+
+        /*так мы вставляем клоны в root */
+        date.setDate(date.getDate() + 1 )
         root.children[i].querySelector("time").innerText = (date+'').slice(0,15)
     }
 }
