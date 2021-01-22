96c77ec14940cad2d3d3d6cd59721a3ce5d741b2
phoenix-02 <50809139+phoenix-02@users.noreply.github.com>
2021-01-22 23:56:29 +0600

    обновление урок 2  комментарии

--- lesson_2/index.js
+++ lesson_2/index.js
@@ -1,8 +1,11 @@
+
+// клонирую 7 элементов- заметок , ставлю разные даты.
+// первым днем всегда будет текущее число
+
 window.onload = function () {
     const todo_list = document.querySelector('.todo_list');
     const root = document.querySelector('#root');
     let date = new Date
-    root.children[0].querySelector("time").innerText = (date+'').slice(0,15)
 
 
     for (let i = 1; i < 7; i++) {
